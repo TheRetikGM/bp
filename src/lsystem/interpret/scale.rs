e21dae4ee0d3c991e7a4ad255c930717d90e9b59
TheRetikGM <theretikgm@gmail.com>
2025-05-13 17:55:28 +0200

    test: Add other scale tests and fix bugs

--- src/lsystem/interpret/scale.rs
+++ src/lsystem/interpret/scale.rs
@@ -64,6 +64,10 @@ impl Scale for BasicScale {
             // Natural minor
             KeySignatureType::Min => match rank {
                 2 | 7 => pitch.move_halftone_up(),
+                8 => {
+                    pitch.move_tone_up();
+                    pitch.move_halftone_up();
+                }
                 _ => pitch.move_tone_up(),
             },
         }
@@ -80,7 +84,11 @@ impl Scale for BasicScale {
                 _ => pitch.move_tone_down(),
             },
             KeySignatureType::Min => match rank {
-                3 | 8 => pitch.move_halftone_down(),
+                0 | 3 | 8 => pitch.move_halftone_down(),
+                11 => {
+                    pitch.move_tone_down();
+                    pitch.move_halftone_down();
+                }
                 _ => pitch.move_tone_down(),
             },
         }
@@ -214,5 +222,145 @@ mod tests {
         assert_eq!(n5, Pitch::new(C, O5, Some(Sharp)));
         assert_eq!(n6, Pitch::new(B, O4, None));
         assert_eq!(n7, Pitch::new(A, O4, None));
+    }
+
+    #[test]
+    fn advance_min() {
+        // Arrange
+        let c_min = KeySignature {
+            ext: ExtNoteName {
+                note_name: C,
+                accidental: None,
+            },
+            signature_type: Min,
+        };
+        let scale = BasicScale { key: c_min };
+        let n1 = Pitch {
+            ext: ExtNoteName {
+                note_name: C,
+                accidental: None,
+            },
+            octave: O4,
+        };
+
+        // Act
+        let n2 = scale.next(&n1);
+        let n3 = scale.next(&n2);
+        let n4 = scale.next(&n3);
+        let n5 = scale.next(&n4);
+        let n6 = scale.next(&n5);
+        let n7 = scale.next(&n6);
+
+        // Assert
+        assert_eq!(n2, Pitch::new(D, O4, None));
+        assert_eq!(n3, Pitch::new(E, O4, Some(Flat)));
+        assert_eq!(n4, Pitch::new(F, O4, None));
+        assert_eq!(n5, Pitch::new(G, O4, None));
+        assert_eq!(n6, Pitch::new(A, O4, Some(Flat)));
+        assert_eq!(n7, Pitch::new(B, O4, None));
+    }
+
+    #[test]
+    fn recede_min() {
+        // Arrange
+        let c_min = KeySignature {
+            ext: ExtNoteName {
+                note_name: C,
+                accidental: None,
+            },
+            signature_type: Min,
+        };
+        let scale = BasicScale { key: c_min };
+        let n1 = Pitch {
+            ext: ExtNoteName {
+                note_name: C,
+                accidental: None,
+            },
+            octave: O5,
+        };
+
+        // Act
+        let n2 = scale.prev(&n1);
+        let n3 = scale.prev(&n2);
+        let n4 = scale.prev(&n3);
+        let n5 = scale.prev(&n4);
+        let n6 = scale.prev(&n5);
+        let n7 = scale.prev(&n6);
+
+        // Assert
+        assert_eq!(n2, Pitch::new(B, O4, None));
+        assert_eq!(n3, Pitch::new(A, O4, Some(Flat)));
+        assert_eq!(n4, Pitch::new(G, O4, None));
+        assert_eq!(n5, Pitch::new(F, O4, None));
+        assert_eq!(n6, Pitch::new(E, O4, Some(Flat)));
+        assert_eq!(n7, Pitch::new(D, O4, None));
+    }
+
+    #[test]
+    fn advance_jazz() {
+        // Arrange
+        let c_jazz = KeySignature {
+            ext: ExtNoteName {
+                note_name: C,
+                accidental: Some(Sharp),
+            },
+            signature_type: Maj,
+        };
+        let scale = JazzLikeScale { key: c_jazz };
+        let n1 = Pitch {
+            ext: ExtNoteName {
+                note_name: C,
+                accidental: Some(Sharp),
+            },
+            octave: O4,
+        };
+
+        // Act
+        let n2 = scale.next(&n1);
+        let n3 = scale.next(&n2);
+        let n4 = scale.next(&n3);
+        let n5 = scale.next(&n4);
+        let n6 = scale.next(&n5);
+
+        // Assert
+        assert_eq!(n2, Pitch::new(D, O4, Some(Sharp)));
+        assert_eq!(n3, Pitch::new(F, O4, Some(Sharp)));
+        assert_eq!(n4, Pitch::new(G, O4, Some(Sharp)));
+        assert_eq!(n5, Pitch::new(A, O4, None));
+        assert_eq!(n6, Pitch::new(A, O4, Some(Sharp)));
+    }
+
+    #[test]
+    fn recede_jazz() {
+        // Arrange
+        let c_jazz = KeySignature {
+            ext: ExtNoteName {
+                note_name: C,
+                accidental: Some(Sharp),
+            },
+            signature_type: Maj,
+        };
+        let scale = JazzLikeScale { key: c_jazz };
+        let n1 = Pitch {
+            ext: ExtNoteName {
+                note_name: C,
+                accidental: Some(Sharp),
+            },
+            octave: O5,
+        };
+
+        // Act
+        let n2 = scale.prev(&n1);
+        let n3 = scale.prev(&n2);
+        let n4 = scale.prev(&n3);
+        let n5 = scale.prev(&n4);
+        let n6 = scale.prev(&n5);
+
+        // Assert
+        assert_eq!(n2, Pitch::new(B, O4, Some(Flat)));
+        assert_eq!(n3, Pitch::new(A, O4, None));
+        assert_eq!(n4, Pitch::new(G, O4, Some(Sharp)));
+        assert_eq!(n5, Pitch::new(F, O4, Some(Sharp)));
+        assert_eq!(n6, Pitch::new(D, O4, Some(Sharp)));
     }
 }
